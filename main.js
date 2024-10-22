9339d3a9c8c85a9f161bbe8c9420b56160fcf112
David Hage <davhage@icloud.com>
2024-10-21 21:18:43 -0600

    segundo commit

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const buttons = document.querySelectorAll('button');
 const input = document.querySelector('input');
 const form = document.getElementById('formId');
+const push = document.getElementById('push');
+const unshift = document.getElementById('unshift');
+const insert = document.getElementById('insert');
+const pop = document.getElementById('pop');
+const shift = document.getElementById('shift');
+const remove = document.getElementById('remove');
+
 let array = [];
 
 const submitForm = (e) => {
@@ -9,11 +16,20 @@ const submitForm = (e) => {
 };
 
 const checkButton = () => {
-    console.log('button clicked');
+  console.log('button clicked');
+};
+
+const pushNumber = () => {
+  array.push(input.value);
+  console.log(array);
+};
+const unshiftNumber = () => {
+  array.unshift(input.value);
+  console.log(array);
 };
 
 form.addEventListener('submit', submitForm);
-buttons.forEach(button => {
-    button.addEventListener('click', checkButton);
-})
+push.addEventListener('click', pushNumber);
 
+
+console.log(array);
